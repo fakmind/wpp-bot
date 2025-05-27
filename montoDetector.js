8b44c977e7fe587df5a37d7f6d7f7a4870389a22
fakmind <fakmind.ba@gmail.com>
2025-05-27 01:45:43 -0300

    Delete montoDetector.js

--- montoDetector.js
+++
@@ -1,9 +0,0 @@
-function detectarMonto(texto) {
-    const match = texto.match(/(\d+(?:[.,]\d{1,2})?)\s*✅/);
-    if (match) {
-        return parseFloat(match[1].replace(',', '.'));
-    }
-    return null;
-}
-
-module.exports = { detectarMonto };
